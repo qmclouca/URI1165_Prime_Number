8e1c166ab975851ca590c908719dcd5597eaee53
qmclouca <qmclouca@gmail.com>
2021-02-02 12:31:36 -0300

    "F:\Google Drive\RODOLFO BORTOLUZZI\ESTUDOS\URIJudge\URI1165\cmake-build-debug\URI1165.exe" 8 54 54 nao eh primo 51 51 nao eh primo 52 52 nao eh primo 57 57 nao eh primo 53 53 eh primo 211 211 eh primo 9 9 nao eh primo 3 3 eh primo
    Process finished with exit code 0
    
    URI Accepted!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int NCases = 0;
+    bool flag = false;
+    std::cin >> NCases;
+    for (int x=0, value=0; x<NCases; x++){
+        std::cin >> value;
+        for(int y=2; y<value; y++){
+            if (value%y == 0){
+                flag = true;
+            }
+        }
+        if (flag == false) {
+            std::cout << value << " eh primo" << std::endl;
+        } else {
+            std::cout << value << " nao eh primo" << std::endl;
+            flag = false;
+        }
+    }
     return 0;
 }
